f9a60a7515ea25254dd92488fce30086bb6477e3
Ezeki Zibzibadze <ez@jok.io>
2014-03-22 09:22:31 -0700

    refactoring

--- Jok.Backgammon/Jok.Backgammon/Controllers/HomeController.cs
+++ Jok.Backgammon/Jok.Backgammon/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using Jok.Backgammon.GameServer;
-using Jok.GameEngine;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -9,46 +7,22 @@ using System.Web.Mvc;
 
 namespace Jok.Backgammon.Controllers
 {
-    public class HomeController : GameControllerBase
+    public class HomeController : Controller
     {
-        #region Properties
-        protected override string AuthorizationUrl
-        {
-            get { return ConfigurationManager.AppSettings["LoginUrl"] + "?returnUrl=" + Request.Url; }
-        }
-
-        protected override string ExitUrl
-        {
-            get { return ConfigurationManager.AppSettings["ExitUrl"]; }
-        }
-
-        protected override int ConnectionsCount
-        {
-            get { return GameHub.Connections.Count; }
-        }
-
-        protected override int TablesCount
-        {
-            get { return GameHub.Tables.Count; }
-        }
-        #endregion
-
-        public override ActionResult Index()
+        public  ActionResult Index()
         {
             return View("Index");
         }
 
-        public override ActionResult Play(string id, string sid, string source)
+        public ActionResult Play(string id, string sid, string source)
         {
-            var result = base.Play(id, sid, source);
-
             // sid რომ არ გამოჩნდეს url-ში
             if (!String.IsNullOrEmpty(sid))
             {
                 return RedirectToAction("Play", new { id = id, source = source, debug = Request.Params["debug"] });
             }
 
-            return result;
+            return View();
         }
     }
 }
