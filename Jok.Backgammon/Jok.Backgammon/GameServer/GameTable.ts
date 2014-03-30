21e3535db24eb75a717a089e380d4e9dd884ea0b
Ezeki Zibzibadze <ez@jok.io>
2014-03-30 07:58:52 -0700

    increesed PLAY_RESERVED_TIME_INTERVAL up to 40 second...

--- Jok.Backgammon/Jok.Backgammon/GameServer/GameTable.ts
+++ Jok.Backgammon/Jok.Backgammon/GameServer/GameTable.ts
@@ -14,7 +14,7 @@ class Commands {
 
 class GameTable extends JP.GameTableBase<GamePlayer> {
 
-    public static PLAY_RESERVED_TIME_INTERVAL = 20 * 1000;
+    public static PLAY_RESERVED_TIME_INTERVAL = 40 * 1000;
 
     public static PLAY_FOR_ROLL_TIME = 20 * 1000;
 
